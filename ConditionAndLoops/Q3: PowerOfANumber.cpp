e8fe8e5446793f9d84da3b1b412d27c7712b48bb
dev nirwal <devnirwal16@gmail.com>
2021-08-21 21:57:57 +0530

    Question 3

--- ConditionAndLoops/Q3: PowerOfANumber.cpp
+++ ConditionAndLoops/Q3: PowerOfANumber.cpp
@@ -1,6 +1,5 @@
 /*
 Find power of a number
-Send Feedback
 Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to print the answer.
 Note : For this question, you can assume that 0 raised to the power of 0 is 1
 */
