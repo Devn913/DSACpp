7b6edbf15c6d50f01e46c1edee43db67d51f7ef8
Dev Nirwal <devnirwal16@gmail.com>
2021-09-15 16:29:38 +0530

    Question 55

--- 9. Recursion/Q55: RemoveX.cpp
+++ 9. Recursion/Q55: RemoveX.cpp
@@ -10,7 +10,7 @@ String S
 using namespace std;
 void removeX(char str[], int counter=0){
     if(str[counter] == '\0'){
-        return ;
+        return;
     }
     if(str[counter] == 'x'){
         int i =counter;
