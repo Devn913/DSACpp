14a858d5d16c247f9b2c601caf8c8d78af70d155
dev <devnirwal16@gmail.com>
2021-11-29 20:02:08 +0530

    Printing a Binary Tree Line wise

---
+++ 15.BinaryTrees/PrintLineWise.cpp
@@ -0,0 +1,88 @@
+/*
+Print a Binary Tree Line Wise
+*/
+#include "iostream"
+#include "queue"
+using namespace std;
+
+template <typename T>
+class BinaryTree
+{
+public:
+    T data;
+    BinaryTree<T> *left;
+    BinaryTree<T> *right;
+    BinaryTree(T val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTree()
+    {
+        delete left;
+        delete right;
+    }
+};
+BinaryTree<int> *inputLineWise()
+{
+    cout << "Enter root data: ";
+    int data;
+    cin >> data;
+    BinaryTree<int> *root = new BinaryTree<int>(data);
+    queue<BinaryTree<int> *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        BinaryTree<int> *front = q.front();
+        q.pop();
+        int val;
+        cout << "Enter left val for " << front->data << " -1 for NULL: ";
+        cin >> val;
+        if (val != -1)
+        {
+            front->left = new BinaryTree<int>(val);
+            q.push(front->left);
+        }
+        cout << "Enter right val for " << front->data << " -1 for NULL: ";
+        cin >> val;
+        if (val != -1)
+        {
+            front->right = new BinaryTree<int>(val);
+            q.push(front->right);
+        }
+    }
+    return root;
+}
+void printLineWise(BinaryTree<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<BinaryTree<int> *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        BinaryTree<int> *front = q.front();
+        q.pop();
+        cout << front->data << ": ";
+
+        if (front->left != NULL)
+        {
+            cout << "L " << front->left->data << " ";
+            q.push(front->left);
+        }
+        if (front->left != NULL)
+        {
+            cout << "R " << front->right->data << " ";
+            q.push(front->right);
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    BinaryTree<int> *root = inputLineWise();
+    printLineWise(root);
+}
